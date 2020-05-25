87a1dd4ad3c874d6d9dd9221c66620f2ccff1310
Ravi Tale <ravitale02@gmail.com>
2020-05-25 13:19:10 +0200

    infinity war initialized

---
+++ src/main/java/com/marvel/avengers/infinitywar/App.java
@@ -0,0 +1,13 @@
+package com.marvel.avengers.infinitywar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
